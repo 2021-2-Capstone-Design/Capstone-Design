fe32ef861d74780d1d7741944ab77c5bf78892f5
bbijun <k36769@cau.ac.kr>
2021-10-31 19:24:26 +0900

    개인작업공간 분리

---
+++ bbijun/code/runvideo.py
@@ -0,0 +1,19 @@
+import vlc
+import time
+
+def run_video():
+    media_player = vlc.MediaPlayer()
+    media_file = 'sample_dance/videoplayback.mp4'
+    media = vlc.Media(media_file)
+    media_player.set_media(media)
+
+    media_player.video_set_scale(1)
+    time.sleep(1)
+    media_player.audio_set_volume(50)
+    time.sleep(1)
+    media_player.play()
+    time.sleep(8)
+    media_player.stop()
+
+run_video()
+
