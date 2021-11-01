5144d8df969e9aebe895eee6d6a7ca6110f9bc11
bbijun <k36769@cau.ac.kr>
2021-11-01 18:59:53 +0900

    no message

---
+++ bbijun/code/takescreenshot.py
@@ -0,0 +1,20 @@
+import vlc
+import time
+
+def take_screenshot():
+    media_player = vlc.MediaPlayer()
+    media_file = './../sample_dance/videoplayback.mp4'
+    media = vlc.Media(media_file)
+    media_player.set_media(media)
+    media_player.set_rate(10)
+    
+    capture_count=10
+    i=0
+    media_player.play()
+    time.sleep(1)
+    while(i<capture_count):
+        time.sleep(0.1)
+        media_player.video_take_snapshot(0,'./../screenshot',0,0)
+        i+=1
+
+take_screenshot()
