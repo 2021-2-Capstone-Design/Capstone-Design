de37ad9ae9750ec4bf14b1aba14239c268fdb019
bbijun <k36769@cau.ac.kr>
2021-11-01 19:41:50 +0900

    change screenshot video

--- bbijun/code/takescreenshot.py
+++ bbijun/code/takescreenshot.py
@@ -3,17 +3,17 @@ import time
 
 def take_screenshot():
     media_player = vlc.MediaPlayer()
-    media_file = './../sample_dance/videoplayback.mp4'
+    media_file = './../../sample_dance/solo.mp4'
     media = vlc.Media(media_file)
     media_player.set_media(media)
     media_player.set_rate(10)
     
-    capture_count=8
+    capture_count=120
     i=0
     media_player.play()
     time.sleep(0.1)
     while(i<capture_count):
-        time.sleep(0.1)
+        time.sleep(0.01)
         media_player.video_take_snapshot(0,'./../screenshot',0,0)
         i+=1
 
