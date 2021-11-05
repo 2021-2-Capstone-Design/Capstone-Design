8b399a66c55cbd95e89c40b1f8ad4a540a934ba3
bbijun <k36769@cau.ac.kr>
2021-11-05 13:36:40 +0900

    video runtime에 맞게 runvideo,takescreenshot 수정

--- bbijun/code/runvideo.py
+++ bbijun/code/runvideo.py
@@ -6,13 +6,11 @@ def run_video():
     media_file = './../sample_dance/videoplayback.mp4'
     media = vlc.Media(media_file)
     media_player.set_media(media)
-
     media_player.video_set_scale(1)
-    time.sleep(1)
+    video_runtime = media_player.get_length()
     media_player.audio_set_volume(50)
-    time.sleep(1)
     media_player.play()
-    time.sleep(8)
+    time.sleep(video_runtime/1000)
     media_player.stop()
 
 run_video()
--- bbijun/code/takescreenshot.py
+++ bbijun/code/takescreenshot.py
@@ -1,19 +1,19 @@
 import vlc
 import time
-
+#1프레임마다 스크린샷 촬영으로 코드 변경
 def take_screenshot():
     media_player = vlc.MediaPlayer()
     media_file = './../sample_dance/videoplayback.mp4'
     media = vlc.Media(media_file)
     media_player.set_media(media)
-    media_player.set_rate(10)
-    
-    capture_count=10
+    media_player.set_rate(1)
+    video_runtime = media_player.get_length()
+    print("video runtime : "+str(video_runtime))
     i=0
     media_player.play()
     time.sleep(1)
-    while(i<capture_count):
-        time.sleep(0.1)
+    while(i<video_runtime):
+        time.sleep(0.001)
         media_player.video_take_snapshot(0,'./../screenshot',0,0)
         i+=1
 
